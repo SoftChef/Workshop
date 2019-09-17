49e471187bac87c76a7b77f25806590b4fbcbcbd
Frank Xiao <frank@softchef.com>
2019-09-17 14:07:12 +0800

    Print Exception

--- tests/faceRekognition.py
+++ tests/faceRekognition.py
@@ -23,25 +23,23 @@ path = os.path.join(currentLocation, file)
 
 def main():
     while True:
+        led.off()
+        buzzer.off()
         os.system('cap 1280 960 8 1 -999 -1 -1')
         with open(path, 'rb') as image_binary:
             try:
                 match_response = rek_client.search_faces_by_image(CollectionId=collectionId, Image={'Bytes': image_binary.read()}, MaxFaces=1, FaceMatchThreshold=90)
                 if match_response['FaceMatches']:
                     led.on()
-                    buzzer.off()
                     print('Hello, ',match_response['FaceMatches'][0]['Face']['ExternalImageId'])
                     print('Similarity: ',match_response['FaceMatches'][0]['Similarity'])
                     print('Confidence: ',match_response['FaceMatches'][0]['Face']['Confidence'])
                     break
                 else:
-                    led.off()
                     buzzer.on()
-                    time.sleep(1)
-                    buzzer.off()
                     print('No faces matched')
-            except:
-                print('No face detected')
+            except Exception as e:
+                print('No face detected', str(e))
         time.sleep(2)
         # Remove the file
         os.remove(path)
